7aff2f8d8751cfdaec7dabc679aa921f023ac2b9
IbrahimHassan80 <59093729+IbrahimHassan80@users.noreply.github.com>
2021-08-05 13:40:16 +0200

    edit login

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -17,7 +17,9 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset('admin-theme/css/sb-admin-2.min.css')}}" rel="stylesheet">
-
+    <style>
+        .bg-login-image{background-image: url({{asset('admin-theme/img/islam.jpg')}})}
+    </style>
 </head>
 
 <body class="bg-gradient-primary">
